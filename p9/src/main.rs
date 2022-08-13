0829f514214230a651cd75aae4411f0a9785ca72
yoshijulas <yoshijulas@live.com>
2022-08-13 17:36:13 -0500

    Triangles

--- p9/src/main.rs
+++ p9/src/main.rs
@@ -6,18 +6,20 @@ fn read_input() -> String {
     input_str
 }
 
+// Triangles
+
 fn main() {
-    let n: i32 = read_input().trim().parse().unwrap();
+    let number: i32 = read_input().trim().parse().unwrap();
 
     let mut res: Vec<i32> = Vec::new();
 
-    for _ in 0..n {
-        let v: Vec<f32> = read_input()
+    for _ in 0..number {
+        let vector_input: Vec<f32> = read_input()
             .split_whitespace()
             .map(|x| x.parse::<f32>().unwrap())
             .collect();
 
-        let (a, b, c) = (v[0], v[1], v[2]);
+        let (a, b, c) = (vector_input[0], vector_input[1], vector_input[2]);
 
         if a + b > c && a + c > b && b + c > a {
             res.push(1);

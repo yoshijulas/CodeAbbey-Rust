f04305ab257495d33d7af0d989d4cab0954c1832
yoshijulas <yoshijulas@live.com>
2022-08-13 17:38:01 -0500

    Array Counters

--- p21/src/main.rs
+++ p21/src/main.rs
@@ -6,6 +6,8 @@ fn read_input() -> String {
     input_str
 }
 
+// Array Counters
+
 fn main() {
     let num = read_input();
     let num: Vec<i32> = num
@@ -13,13 +15,14 @@ fn main() {
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
-    let mut res: Vec<i32> = vec![0; num[1] as usize];
+    let mut res: Vec<i32> = vec![0; num[1].try_into().unwrap()];
 
     let input = read_input();
     let input: Vec<i32> = input
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
+
 
     for i in input {
         res[i as usize - 1] += 1;

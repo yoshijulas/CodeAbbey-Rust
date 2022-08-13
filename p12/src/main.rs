cd0ffbca463683ea77ad6de24a1eecb4dcb8c9d4
yoshijulas <yoshijulas@live.com>
2022-08-13 17:36:50 -0500

    Modulo and time difference

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -6,19 +6,21 @@ fn read_input() -> String {
     input_str
 }
 
+// Modulo and time difference
+
 fn main() {
-    let n = read_input().trim().parse::<usize>().unwrap();
+    let number = read_input().trim().parse::<usize>().unwrap();
 
     let mut res: Vec<(i32, i32, i32, i32)> = Vec::new();
 
-    for _ in 0..n {
-        let v: Vec<i32> = read_input()
+    for _ in 0..number {
+        let vector_input: Vec<i32> = read_input()
             .split_whitespace()
             .map(|x| x.parse::<i32>().unwrap())
             .collect();
 
         let (day1, hour1, min1, mut sec1, day2, hour2, min2, mut sec2) =
-            (v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
+            (vector_input[0], vector_input[1], vector_input[2], vector_input[3], vector_input[4], vector_input[5], vector_input[6], vector_input[7]);
 
         sec1 += (day1 * 86400) + (hour1 * 3600) + (min1 * 60);
         sec2 += (day2 * 86400) + (hour2 * 3600) + (min2 * 60);
@@ -27,15 +29,15 @@ fn main() {
         let mut dif = sec2 - sec1;
 
         //convert to days, hours, minutes and seconds
-        let a = dif / 86400;
+        let days_number = dif / 86400;
         dif %= 86400;
-        let b = dif / 3600;
+        let hours_number = dif / 3600;
         dif %= 3600;
-        let c = dif / 60;
+        let minutes_number = dif / 60;
         dif %= 60;
-        let d = dif;
+        let seconds_number = dif;
 
-        res.push((a, b, c, d));
+        res.push((days_number, hours_number, minutes_number, seconds_number));
     }
 
     for i in res {

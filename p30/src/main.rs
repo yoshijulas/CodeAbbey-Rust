e260913645ab2370ac60921591032ef349d617e9
yoshijulas <yoshijulas@live.com>
2022-08-13 17:39:22 -0500

    Reverse String

---
+++ p30/src/main.rs
@@ -0,0 +1,15 @@
+fn read_input() -> String {
+    let mut input_str = String::new();
+    std::io::stdin()
+        .read_line(&mut input_str)
+        .expect("Failed to read line");
+    input_str
+}
+
+// Reverse String
+
+fn main() {
+    let input: String = read_input();
+    let rev_input: String = input.chars().rev().collect();
+    print!("{}", rev_input);
+}

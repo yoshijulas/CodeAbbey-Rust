9e9e1b3d58da68c5943a7645555099931f3e8fbd
yoshijulas <yoshijulas@live.com>
2022-10-23 22:47:06 -0500

    Bubble Sort

--- p27/src/main.rs
+++ p27/src/main.rs
@@ -6,6 +6,23 @@ fn read_input() -> String {
     input_str
 }
 
+// Bubble Sort
+
+fn main() {
+    let _num = read_input();
+
+    let input_vec: Vec<i32> = read_input()
+        .split_whitespace()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let (_sorted_vec, passes, counter, _max_counter) = bubble_sort(input_vec);
+
+    println!("{passes} {counter}");
+    // println!("{:?}", _sorted_vec);
+    // println!("{}", max_counter);
+}
+
 fn bubble_sort(mut input_vec: Vec<i32>) -> (Vec<i32>, i32, i32, i32) {
     let mut counter = 0;
 
@@ -27,25 +44,8 @@ fn bubble_sort(mut input_vec: Vec<i32>) -> (Vec<i32>, i32, i32, i32) {
             max_counter += 1;
         }
         n -= 1; // one less element to check, improves performance, numeric triangle () every pass removed
-        // 374 -> 238 for 23 values
+                // 374 -> 238 for 23 values
         passes += 1;
     }
     (input_vec, passes, counter, max_counter)
-}
-
-// Bubble Sort
-
-fn main() {
-    let _num = read_input();
-
-    let input_vec: Vec<i32> = read_input()
-        .split_whitespace()
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect();
-
-    let (_sorted_vec, passes, counter, _max_counter) = bubble_sort(input_vec);
-
-    println!("{} {}", passes, counter);
-    // println!("{:?}", _sorted_vec);
-    // println!("{}", max_counter);
 }
